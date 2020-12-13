8839b93f52337f81a3e11c5b3c6b16d3437a1a09
Tim LeMaster <tim@thelemasters.net>
2020-12-12 22:05:48 -0500

    Use struct to improve readability

--- day12/solve.rb
+++ day12/solve.rb
@@ -3,35 +3,45 @@
 HEADINGS = { 0 => "E", 90 => "S", 180 => "W", 270 => "N" }
 
 ROTATIONS = {
-  0   => ->(we, wn, e, n) { return  we,  wn, e, n },
-  90  => ->(we, wn, e, n) { return  wn, -we, e, n },
-  180 => ->(we, wn, e, n) { return -we, -wn, e, n },
-  270 => ->(we, wn, e, n) { return -wn,  we, e, n }
+  0   => ->(ship) { },
+  90  => ->(ship) { ship.update_waypoints(ship.waypoint_north, -ship.waypoint_east) },
+  180 => ->(ship) { ship.update_waypoints(-ship.waypoint_east, -ship.waypoint_north) },
+  270 => ->(ship) { ship.update_waypoints(-ship.waypoint_north, ship.waypoint_east) }
 }
 
 HEADING_COMMANDS = {
-  "N" => ->(we, wn, value, e, n) { return we, wn, e, n + value },
-  "E" => ->(we, wn, value, e, n) { return we, wn, e + value, n },
-  "S" => ->(we, wn, value, e, n) { return we, wn, e, n - value },
-  "W" => ->(we, wn, value, e, n) { return we, wn, e - value, n },
-  "F" => ->(we, wn, value, e, n) { return HEADING_COMMANDS[HEADINGS[we]].call(we, wn, value, e, n) },
-  "R" => ->(we, wn, value, e, n) { return (we + value >= 360 ? we + value - 360 : we + value), wn , e, n },
-  "L" => ->(we, wn, value, e, n) { return HEADING_COMMANDS["R"].call(we, wn, 360 - value, e, n) },
+  "N" => ->(ship, value) { ship.north += value },
+  "E" => ->(ship, value) { ship.east += value },
+  "S" => ->(ship, value) { ship.north -= value },
+  "W" => ->(ship, value) { ship.east -= value },
+  "F" => ->(ship, value) { HEADING_COMMANDS[HEADINGS[ship.heading]].call(ship, value) },
+  "R" => ->(ship, value) { ship.heading += value; ship.heading -= 360 if ship.heading >= 360 },
+  "L" => ->(ship, value) { HEADING_COMMANDS["R"].call(ship, 360 - value) },
 }
 
 WAYPOINT_COMMANDS = {
-  "N" => ->(we, wn, value, e, n) { return we, wn + value, e, n },
-  "E" => ->(we, wn, value, e, n) { return we + value, wn, e, n },
-  "S" => ->(we, wn, value, e, n) { return we, wn - value, e, n },
-  "W" => ->(we, wn, value, e, n) { return we - value, wn, e, n },
-  "F" => ->(we, wn, value, e, n) { return we, wn, (e + (we * value)), (n + (wn * value)) },
-  "R" => ->(we, wn, value, e, n) { return ROTATIONS[value].call(we, wn, e, n) },
-  "L" => ->(we, wn, value, e, n) { return WAYPOINT_COMMANDS["R"].call(we, wn, 360 - value, e, n) },
+  "N" => ->(ship, value) { ship.waypoint_north += value },
+  "E" => ->(ship, value) { ship.waypoint_east += value },
+  "S" => ->(ship, value) { ship.waypoint_north -= value },
+  "W" => ->(ship, value) { ship.waypoint_east -= value },
+  "F" => ->(ship, value) { ship.east += ship.waypoint_east * value; ship.north += ship.waypoint_north * value },
+  "R" => ->(ship, value) { ROTATIONS[value].call(ship) },
+  "L" => ->(ship, value) { WAYPOINT_COMMANDS["R"].call(ship, 360 - value) },
 }
 
 input = File.readlines("input.txt", :chomp => true)
+solve = ->(commands, ship) { input.reduce(ship) { |ship, line| commands[line[0]].call(ship, line[1..].to_i); ship } }
 
-solve = ->(data, commands, values) { data.reduce(values) { |values, line| values = commands[line[0]].call(values[0], values[1], line[1..].to_i, values[2], values[3]) }[2, 2].sum {|e| e.abs} }
+Ship = Struct.new(:heading, :waypoint_east, :waypoint_north, :east, :north) do
+  def manhattan_distance
+    east.abs + north.abs
+  end
 
-puts solve.call(input, HEADING_COMMANDS, [0, 0, 0, 0])
-puts solve.call(input, WAYPOINT_COMMANDS, [0, 0, 10, 1])
+  def update_waypoints(we, wn)
+    self.waypoint_east = we
+    self.waypoint_north = wn
+  end
+end
+
+puts solve.call(HEADING_COMMANDS, Ship.new(0, 0, 0, 0, 0)).manhattan_distance
+puts solve.call(WAYPOINT_COMMANDS, Ship.new(0, 10, 1, 0, 0)).manhattan_distance
